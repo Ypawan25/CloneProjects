eac47593d9841e0112a0fab6d2a55d2c175b5def
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-30 22:45:28 +0530

    yotube intial updates

---
+++ WebsiteClones/Youtube/youtube/src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+function Navbar() {
+  return (
+    <div>
+     <div>
+            <div><img src="./images/lines.jpg" alt="line"/></div>
+            <div><img src="./images/logo.jpg" alt="line"/></div>
+     </div>
+     <div>
+        <input></input>
+        <button></button>
+     </div>
+     <div></div>
+    </div>
+  )
+}
+
+export default Navbar
